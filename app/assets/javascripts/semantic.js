64156f170762393d6fb36b788a38299ba77e093c
Aya <ayaaa.okzk@gmail.com>
2025-12-02 09:25:36 -0800

    add scroll bottom function

--- app/assets/javascripts/semantic.js
+++ app/assets/javascripts/semantic.js
@@ -1,12 +1,16 @@
 //= require jquery
 //= require semantic-ui
 
-$(document).on("turbo:load", function () {
-	$(".ui.dropdown").dropdown();
-});
+scroll_bottom = function () {
+	if ($("#messages").length) {
+		$("#messages").scrollTop($("#messages")[0].scrollHeight);
+	}
+};
 
 $(document).on("turbo:load", function () {
+	$(".ui.dropdown").dropdown();
 	$(".message .close").on("click", function () {
 		$(this).closest(".message").transition("fade");
 	});
+	scroll_bottom();
 });
