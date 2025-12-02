da324fbbe71e48072965050311da35bc5d5c3d5d
Aya <ayaaa.okzk@gmail.com>
2025-12-02 09:13:45 -0800

    add channel

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,19 @@ class MessagesController < ApplicationController
     message = current_user.messages.build(message_params)
 
     if message.save
+      ActionCable.server.broadcast "chatroom_channel",
+                                   { mod_message: message_render(message) }
       redirect_to root_path
     end
   end
-
 
   private
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render_to_string(partial: "messages/message", locals: { message: message })
   end
 end
