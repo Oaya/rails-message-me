59ffc83db7e26cda644d9fb05909f5c205e68910
Aya <ayaaa.okzk@gmail.com>
2025-12-01 20:43:53 -0800

    add login

--- app/assets/javascripts/semantic.js
+++ app/assets/javascripts/semantic.js
@@ -4,3 +4,9 @@
 $(document).on("turbo:load", function () {
 	$(".ui.dropdown").dropdown();
 });
+
+$(document).on("turbo:load", function () {
+	$(".message .close").on("click", function () {
+		$(this).closest(".message").transition("fade");
+	});
+});
